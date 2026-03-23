5f4f82d0c62770282ea2c3a6c504d228aa0ddad2
joaquimcascao <joaquimmmcascao@gmail.com>
2026-03-23 07:17:18 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { LoginPage } from "./components/LoginPage";
+import { PokedexHomepage } from "./components/Pokedex";
+import { useState } from "react";
+
+function App() {
+	const [page] = useState('login')
+	switch (page) {
+		case `login`:
+			return (
+				<LoginPage />
+			)
+		case `homepage`:
+			return (
+				<PokedexHomepage />
+			);
+	}
+}
+
+export default App;
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,16 @@
+export const LoginPage = () => {
+    return (
+        <div className=" bg-zinc-900 min-h-screen flex items-center justify-center">
+            <div className="bg-zinc-800 flex flex-col justify-center gap-2 p-4 rounded-md text-zinc-50">
+                <div className="flex justify-center text-zinc-50">
+                    <h1 className="text-3xl font-bold">LOGIN</h1>
+                </div>
+                <textarea rows={1} placeholder="Username" className="outline-0 resize-none bg-zinc-900 p-2 rounded-md" />
+                <textarea rows={1} placeholder="Password" className="outline-0 resize-none bg-zinc-900 p-2 rounded-md" />
+                <div className="flex flex-col justify-center">
+                    <button className="cursor-pointer bg-red-600 text-zinc-50 p-2 rounded-md">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
