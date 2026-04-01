8d9198f952174be4271302828addf3bb3cc77204
joaquimcascao <joaquimmmcascao@gmail.com>
2026-04-01 09:04:14 -0300

    Add localhost origin to cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 || 5173
 
 app.use(cors({
-  origin: 'https://pokedex-joaquim.vercel.app'
+  origin: [
+    'https://pokedex-joaquim.vercel.app',
+    'http://localhost:5173'
+  ]
 }))
 app.use(express.json())
 
