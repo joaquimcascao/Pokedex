983589ef712ba0ffb171961ae52c209caaec0b74
joaquimcascao <joaquimmmcascao@gmail.com>
2026-05-03 18:49:27 -0300

    Fix Render error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import pokemonRoutes from './routes/pokemonRoutes.js';
+import { Router } from 'express';
+import { getPokemon } from '../controllers/pokemonController.js';
+
+const router = Router();
+
+router.post('/pokemon', getPokemon);
+router.all('/keepalive', (req, res) => res.status(200).send('OK'));
+
+export default router;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,9 +21,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api', pokemonRoutes);
-
-router.post('/pokemon', getPokemon);
-router.all('/keepalive', (req, res) => res.status(200).send('OK'));
 
 app.listen(PORT, () => {
     console.log(`Backend running: http://localhost:${PORT}`);
